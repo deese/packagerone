b23af24ad3f03e8e93fc40be56ba6653c28e659d
deese <deese2k@gmail.com>
2025-05-29 17:58:25 +0200

    Adding the proper fzf-pkg script

---
+++ scripts/fzf-pkg.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+source environ.sh
+
+REPO="https://github.com/junegunn/fzf"
+DPKG_BASENAME="fzf"
+DPKG_VERSION="0.62.0"
+ORIG_FILENAME="fzf-$DPKG_VERSION-linux_$DPKG_ARCH.tar.gz"
+URL="$REPO/releases/download/v$DPKG_VERSION/$ORIG_FILENAME"
+DPKG_DIR="fzf-v$DPKG_VERSION-$TARGET_ARCH"
+DPKG_CONFLICTS=""
+DPKG_NAME="${DPKG_BASENAME}_${DPKG_VERSION}_${DPKG_ARCH}.deb"
+
+
+wget $URL
+
+if [ ! -f $ORIG_FILENAME ]; then
+  echo Error downloading file.
+  exit
+fi
+tar zxvf $ORIG_FILENAME
+
+
+install -Dm755 "fzf" "${DPKG_DIR}/usr/bin/fzf"
+
+mkdir -p "${DPKG_DIR}/DEBIAN"
+cat > "${DPKG_DIR}/DEBIAN/control" << EOF
+Package: ${DPKG_BASENAME}
+Version: ${DPKG_VERSION}
+Section: utils
+Priority: optional
+Maintainer: ${MAINTAINER}
+Homepage: ${REPO}
+Architecture: ${DPKG_ARCH}
+Description: fzf is a general-purpose command-line fuzzy finder.
+  It's an interactive filter program for any kind of list; files, command history, processes, hostnames, bookmarks,
+  git commits, etc. It implements a "fuzzy" matching algorithm, so you can quickly type in patterns with omitted
+  characters and still get the results you want.
+EOF
+
+mkdir -p dist
+DPKG_PATH="./dist/$DPKG_NAME"
+
+fakeroot dpkg-deb --build "${DPKG_DIR}" "${DPKG_PATH}"
+rm -fr fzf ${DPKG_DIR} $ORIG_FILENAME
